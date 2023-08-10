76ed13f3828728791a61746320337cb4ee22664f
MarkHXB <saslik98@outlook.com>
2023-08-10 22:25:38 +0200

    Added maui project

---
+++ Saturn.Mobile/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Saturn.Mobile
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Saturn.Mobile/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Saturn.Mobile
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
